bd2f8fb2d6e60b5543887da90f18828ceabc994d
talal-atiq <iamtalalatique@gmail.com>
2024-07-17 02:47:46 +0500

    Add files via upload

---
+++ Day 1 - Band Name Generator/Day 1 - Band Name Generator.py
@@ -0,0 +1,6 @@
+#Day1: Band Name Generator
+city = input("Enter the name of city you were born in?\n")
+pet = input("What is/was the name of your pet?\n")
+band = city + " " + pet
+print("Your band name could be" ,band)
+input()
